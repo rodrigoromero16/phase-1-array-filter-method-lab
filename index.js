47c697aca782b0bd186d7ee07a95faf9f028db3d
Rodrigo <rromero22398@gmail.com>
2023-04-18 19:00:51 -0400

    array filter method

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, name){
+    const newCollection = [];
+    for (const driver of drivers){
+        if (driver.toLowerCase() === name.toLowerCase()){
+            newCollection.push(driver);
+        }
+    }
+    return newCollection;
+}
+function fuzzyMatch(drivers, partialName) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(partialName.toLowerCase()));
+}
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
+      
